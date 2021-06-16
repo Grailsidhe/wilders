1c271d6aa1a004ea2bf2fd8954e167d37236c3cb
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-06-16 17:26:16 +0200

    Uploading created files

---
+++ index.js
@@ -0,0 +1,6 @@
+const wilders = require("./wilders");
+const chalk = require("chalk");
+
+ console.log(chalk.blue(
+    `And the wilders are ${wilders.first}, ${wilders.second}, ${wilders.third}, ${wilders.fourth}`
+));
---
+++ wilders.js
@@ -0,0 +1,8 @@
+const wilders = {
+    first: "Victor",
+    second: "Alex",
+    third: "Pilar",
+    fourth: "Natalie"
+};
+
+module.exports = wilders;
